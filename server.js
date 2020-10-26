8a255782eaa014bcef7ca359926e16642ca30819
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2020-10-26 11:19:29 +0100

    Database connectivity with Mongo and Mongoose

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-//Route files
-const categories = require("./routes/categories");
+const connectDB = require("./config/db");
 
 // Load env variables
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+//Route files
+const categories = require("./routes/categories");
 
 const app = express();
 
@@ -20,7 +24,13 @@ app.use("/api/v1/categories", categories);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//Handle unhandles promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
