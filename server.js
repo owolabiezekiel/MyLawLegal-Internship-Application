5e7b860b0ffacdaaf5a4a7d89edc9139d3bc5ae4
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2020-10-28 03:41:57 +0100

    added HTML documentation with docgen

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
@@ -26,6 +27,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount routers
 app.use("/api/v1/categories", categories);
